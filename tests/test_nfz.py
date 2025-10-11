0760d4218e75695280d59e9b23e889ac1c8ac5aa
Hager <hagerabuanga@gmail.com>
2025-10-11 11:50:31 +0300

    runnig tests with correct results

--- tests/test_nfz.py
+++ tests/test_nfz.py
@@ -3,5 +3,5 @@ from src.fast_api_airguardian.task import is_in_nfz
 def test_in_nfz():
     """Test drone positions inside No-Fly-Zone"""
 
-    assert is_in_nfz(100, 110) == False
+    assert is_in_nfz(100, 110) == True
     assert is_in_nfz(0,0) == True
