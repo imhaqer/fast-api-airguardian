d4f31cf02ccadf29d9506f25a20523be7f4caa10
Hager <hagerabuanga@gmail.com>
2025-10-12 16:04:47 +0300

    update and add mock responses for drone API

--- tests/test_nfz.py
+++ tests/test_nfz.py
@@ -1,7 +1,51 @@
 from src.fast_api_airguardian.task import is_in_nfz
+from src.fast_api_airguardian.main import app
+from src.fast_api_airguardian.main import get_drones
+from httpx import AsyncClient
+from fastapi.testclient import TestClient
+import httpx
+import pytest
 
-def test_in_nfz():
-    """Test drone positions inside No-Fly-Zone"""
+client = TestClient(app)
 
-    assert is_in_nfz(100, 110) == True
-    assert is_in_nfz(0,0) == True
+@pytest.mark.parametrize("position_x, position_y, expected", [
+    (110, 100, True),
+    (0,0, True),
+    (-1001, 200, False),
+    (1000,1000, False),
+    (999, 999, False),
+    (200, 999, False),
+    (25355345, 8999999, False),
+    (-656, -15, True),
+    (1000, 0.1, False),
+    (0, 1000, True),
+    (1000, 1, False)
+])
+
+def test_is_in_nfz(position_x, position_y, expected):
+    assert is_in_nfz(position_x, position_y) == expected
+
+
+# @pytest.mark.asyncio no need for async 
+def test_get_drones(mocker, fake_response, fake_data):
+    mocker.patch("src.fast_api_airguardian.main.httpx.AsyncClient.get",
+                return_value=fake_response)
+    response = client.get("/drones") # sync call
+    assert response.status_code == 200
+    assert response.json() == fake_response.json() #compare
+
+
+def test_get_drone_fail(mocker, fake_error_response):
+    mocker.patch("src.fast_api_airguardian.main.httpx.AsyncClient.get",
+                return_value=fake_error_response)
+    response = client.get("/drones") # sync call
+    assert response.status_code == 503
+    assert response.json() == {"detail": "Drone data unavailable"}
+
+
+def test_invalid_drone(mocker, fake_invalid_response):
+    mocker.patch("src.fast_api_airguardian.main.httpx.AsyncClient.get",
+                return_value=fake_invalid_response)
+    response = client.get("/drones") # sync call
+    assert response.status_code == 500
+    assert response.json() == {"detail": "Invalid drone data received"}
