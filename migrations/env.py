0518f2b85a429be4e46fdbb8a81541fefa10c9a1
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2026-02-24 11:38:32 +0000

    configure alembic migrations

---
+++ migrations/env.py
@@ -0,0 +1,48 @@
+from logging.config import fileConfig
+from sqlalchemy import create_engine, pool
+from alembic import context
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+from fast_api_airguardian.database import Base
+from fast_api_airguardian import model  # noqa: F401
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    context.configure(
+        url=os.environ["DATABASE_URL_SYNC"],
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    connectable = create_engine(
+        os.environ["DATABASE_URL_SYNC"],
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
